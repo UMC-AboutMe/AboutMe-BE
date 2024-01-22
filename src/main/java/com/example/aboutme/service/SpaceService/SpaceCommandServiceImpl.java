2421a5ef6da50e2f344be4256434b17ecbdda596
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-01-22 23:24:56 +0900

    feat: space service 추가

---
+++ src/main/java/com/example/aboutme/service/SpaceService/SpaceCommandServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.aboutme.service.SpaceService;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class SpaceCommandServiceImpl implements SpaceCommandService{
+}
