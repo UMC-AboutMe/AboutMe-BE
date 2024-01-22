25f45350a29e9d16b05ccec151f080de45abb46c
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-01-22 23:53:41 +0900

    feat: space 관련 DTO 생성

---
+++ src/main/java/com/example/aboutme/app/dto/SpaceRequest.java
@@ -0,0 +1,11 @@
+package com.example.aboutme.app.dto;
+
+import lombok.Getter;
+
+public class SpaceRequest {
+    @Getter
+    public static class JoinDTO {
+        private String nickname;
+        private Integer characterType;
+    }
+}
