a8a698553ef403471470237b8d85e6bb39287e0e
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-01-23 22:51:19 +0900

    feat: @Transactional 추가

--- src/main/java/com/example/aboutme/service/SpaceService/SpaceCommandServiceImpl.java
+++ src/main/java/com/example/aboutme/service/SpaceService/SpaceCommandServiceImpl.java
@@ -7,7 +7,10 @@ import com.example.aboutme.repository.SpaceRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
+@Transactional
 @AllArgsConstructor
 public class SpaceCommandServiceImpl implements SpaceCommandService{
     private final SpaceRepository spaceRepository;
