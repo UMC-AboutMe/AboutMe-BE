07092c6abe5224a062006eaf47cb0ab954eb977d
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-01-26 18:26:17 +0900

    feat: 불필요한 Response DTO 삭제

--- src/main/java/com/example/aboutme/app/dto/SpaceResponse.java
+++ src/main/java/com/example/aboutme/app/dto/SpaceResponse.java
@@ -34,7 +34,6 @@ public class SpaceResponse {
         private Mood mood;
         private String musicUrl;
         private String statusMessage;
-        private Member member;
         private List<SpaceImage> spaceImageList;
         private List<Plan> planList;
     }
--- src/main/java/com/example/aboutme/repository/SpaceRepository.java
+++ src/main/java/com/example/aboutme/repository/SpaceRepository.java
@@ -1,8 +1,10 @@
 package com.example.aboutme.repository;
 
+import com.example.aboutme.domain.Member;
 import com.example.aboutme.domain.Space;
+import com.example.aboutme.service.MemberService.MemberService;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SpaceRepository extends JpaRepository<Space, Long> {
-
+    Space findByMember_Id(Long memberId);
 }
