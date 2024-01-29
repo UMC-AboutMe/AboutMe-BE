a99e593750e1a8499251088d1c4bb0a990a723ca
201910806ByunSungHo <bsh4237@gmail.com>
2024-01-30 01:05:13 +0900

    Update KakaoServiceImpl.java

--- src/main/java/com/example/aboutme/Login/service/KakaoServiceImpl.java
+++ src/main/java/com/example/aboutme/Login/service/KakaoServiceImpl.java
@@ -14,12 +14,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class KakaoServiceImpl implements KakaoService {
     private final MemberRepository memberRepository;
 
