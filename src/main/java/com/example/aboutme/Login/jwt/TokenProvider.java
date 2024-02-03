4c4566e407e382cd513f874e59ff74370270a91d
201910806ByunSungHo <bsh4237@gmail.com>
2024-02-03 12:58:48 +0900

    update/access token Expiretime 변경

--- src/main/java/com/example/aboutme/Login/jwt/TokenProvider.java
+++ src/main/java/com/example/aboutme/Login/jwt/TokenProvider.java
@@ -26,7 +26,7 @@ import java.util.stream.Collectors;
 @Component
 public class TokenProvider implements InitializingBean {
     private final String secret = "a0484ca954f0b57df32c48691a9570cc01f6117ac8f270b89c33ab84cd53fd15e095d443dd06ea25f3860b4f70762ec53941a46826cbe112c5c7e3ea6cb65d05";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 3000;
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;
 //    private final Key key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
     private Key key;
