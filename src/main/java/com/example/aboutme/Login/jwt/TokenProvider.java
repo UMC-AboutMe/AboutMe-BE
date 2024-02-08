7c5332b18a9d8e98c54e59f4fb4e3cecfff995c3
201910806ByunSungHo <bsh4237@gmail.com>
2024-02-08 21:28:46 +0900

    Update TokenProvider.java

--- src/main/java/com/example/aboutme/Login/jwt/TokenProvider.java
+++ src/main/java/com/example/aboutme/Login/jwt/TokenProvider.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.InitializingBean;
 //import org.springframework.security.core.GrantedAuthority;
 //import org.springframework.security.core.authority.SimpleGrantedAuthority;
 //import org.springframework.security.core.userdetails.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 
@@ -25,8 +26,9 @@ import java.util.stream.Collectors;
 
 @Component
 public class TokenProvider implements InitializingBean {
-    private final String secret = "a0484ca954f0b57df32c48691a9570cc01f6117ac8f270b89c33ab84cd53fd15e095d443dd06ea25f3860b4f70762ec53941a46826cbe112c5c7e3ea6cb65d05";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 3000;
+    @Value("${spring.security.jwt.secret}")
+    private String secret;
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 10000000 * 1000 * 60 * 3000;
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;
 //    private final Key key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
     private Key key;
